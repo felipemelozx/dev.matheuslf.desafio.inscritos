10cff33017a46486cb526af3bc5689bb5612f842
Felipe Melo <felipemelozx@gmail.com>
2025-10-20 12:02:30 -0300

    feat: Adição de manipuladores de exceções para CredentialException e DataIntegrityViolationException no ControllerExceptionHandler

--- src/main/java/dev/matheuslf/desafio/inscritos/exception/ControllerExceptionHandler.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/exception/ControllerExceptionHandler.java
@@ -5,6 +5,7 @@ import dev.matheuslf.desafio.inscritos.utils.ApiResponse;
 import dev.matheuslf.desafio.inscritos.utils.CustomFieldError;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.security.auth.login.CredentialException;
 import java.util.List;
 
 @ControllerAdvice
@@ -69,5 +71,38 @@ public class ControllerExceptionHandler {
         .build();
 
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+  }
+
+  @ExceptionHandler(CredentialException.class)
+  public ResponseEntity<ApiResponse<CustomFieldError>> handleCredentialException(CredentialException ex) {
+    var body = ApiResponse.<CustomFieldError>failure()
+        .message("Error ao tentar fazer login.")
+        .errors(new CustomFieldError("Password" ,ex.getMessage()))
+        .build();
+    return ResponseEntity
+        .status(HttpStatus.UNAUTHORIZED)
+        .body(body);
+  }
+
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ResponseEntity<ApiResponse<CustomFieldError>> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+    String defaultMessage = "Erro de integridade no banco de dados.";
+    String causeMessage = ex.getLocalizedMessage();
+
+    String fieldName = "Geral";
+    if (ex.getCause() != null && ex.getCause().getCause() != null) {
+      String dbMessage = ex.getCause().getCause().getMessage();
+      if (dbMessage != null && dbMessage.contains("email")) {
+        fieldName = "Email";
+        causeMessage = "Este e-mail já está cadastrado!";
+      }
+    }
+
+    var body = ApiResponse.<CustomFieldError>failure()
+        .message(defaultMessage)
+        .errors(new CustomFieldError(fieldName, causeMessage))
+        .build();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
   }
 }
